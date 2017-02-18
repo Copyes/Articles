92a3374fd9e47cbdb96f74ad42397849e2379f2b
lip.fan <lip.fan@husor.com>
2017-02-18 17:56:23 +0800

    本地笔记本

---
+++ note/src/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="app">
+    <n-header @tools="changePages"></n-header>
+  </div>
+</template>
+
+<script type="text/babel">
+import nHeader from './components/Header.vue';
+import nTools from './components/Tools.vue';
+export default {
+  name: 'app',
+  data(){
+    return {
+        tools: false,
+        showTable: false
+    }
+  },
+  methods: {
+    changePages(){
+        if(this.showTable){
+            this.showTable = !this.showTable;
+        }else{
+            this.tools = !this.tools
+        }
+    }
+  },
+  components: {
+      nHeader
+  }
+}
+</script>
+
+<style lang="less">
+    html,body,ul,li,input{
+        margin:0;
+        padding:0;
+    }
+    body{
+        font-size: 16px;
+    }
+    input,button{
+        -webkit-tap-highlight-color: transparent;
+    }
+    input[type=text]{
+        -webkit-appearance: none;
+    }
+    button{
+        padding:7px 0;
+        outline: none;
+        text-align: center;
+        border-radius: 4px;
+        box-sizing: border-box;
+        cursor: pointer;
+    }
+    body,#app{
+        width:100%;
+        overflow-x: hidden;
+    }
+    ul{
+        list-style: none;
+    }
+</style>
---
+++ note/src/components/Tools.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="event-tools" :class="{'show-event-tools':isShow}">
+        <ul class="tools-sidebar">
+            <li>
+                <button class="tools-btn" @click="backupData">打印数据</button>
+            </li>
+            <li>
+                <button class="tools-btn" @click="openTable">编辑数据</button>
+            </li>
+            <li>
+                <button class="tools-btn" @click="showDialog">清空数据</button>
+            </li>
+        </ul>
+    </div>
+</template>
+<script>
+    export default {
+        data () {
+            return {
+
+            }
+        },
+        props:['isShow'],
+        methods: {
+            showDialog(){
+                this.$emit('cleardialog');
+            },
+            backupData () {
+                console.log(localStorage.getItem('lx_notepad'));
+            },
+            openTable(){
+                this.$emit('opentable');
+            }
+        }
+    }
+</script>
+<style lang="less">
+    
+</style>
