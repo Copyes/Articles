315de11c5748eb76ebec303cb5545ffa4c391dbd
Copyes <fan_545697167@qq.com>
2017-04-17 22:26:52 +0800

    demo3

---
+++ JSFun/CodeLearning/event/demo3.js
@@ -0,0 +1,39 @@
+console.log('start')
+
+const interval = setInterval(() => {  
+  console.log('setInterval')
+}, 0)
+
+setTimeout(() => {  
+  console.log('setTimeout 1')
+  Promise.resolve()
+      .then(() => {
+        console.log('promise 3')
+      })
+      .then(() => {
+        console.log('promise 4')
+      })
+      .then(() => {
+        setTimeout(() => {
+          console.log('setTimeout 2')
+          Promise.resolve()
+              .then(() => {
+                console.log('promise 5')
+              })
+              .then(() => {
+                console.log('promise 6')
+              })
+              .then(() => {
+                clearInterval(interval)
+              })
+        }, 0)
+      })
+}, 0)
+
+Promise.resolve()
+    .then(() => {  
+        console.log('promise 1')
+    })
+    .then(() => {
+        console.log('promise 2')
+    })
