cd9263f2c7cd296f66661b44900f3edbcb3a37e8
Copyes <fan_545697167@qq.com>
2018-03-07 12:52:35 +0800

    :sparkles: add proxt server tests

---
+++ test-cross/serverProxy/server.js
@@ -0,0 +1,24 @@
+const url = require('url')
+const http = require('http')
+const https = require('https')
+
+http
+  .createServer((req, res) => {
+    const path = url.parse(req.url).path.slice(1)
+    if (path === 'topics') {
+      https.get('https://cnodejs.org/api/v1/topics', resp => {
+        let data = ''
+        resp.on('data', chunk => {
+          data += chunk
+        })
+        resp.on('end', () => {
+          res.writeHead(200, {
+            'Content-Type': 'application/json; charset=utf-8'
+          })
+          res.end(data)
+        })
+      })
+    }
+  })
+  .listen(3000, '127.0.0.1')
+console.log('The server is listening to 127.0.0.0')
